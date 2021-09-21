db1c3a7f57dabf1986817cc80bad5c7324c588d2
Aleks <kuriatae@mail.ru>
2021-09-21 22:30:21 +0700

    Создание адаптера RecyclerView

---
+++ app/src/main/java/com/example/popularlibraries/UsersRVAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.popularlibraries
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.popularlibraries.databinding.ItemBinding
+
+class UsersRVAdapter(
+    private val presenter: IUserListPresenter
+) : RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(
+            ItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        ).apply {
+            itemView.setOnClickListener { presenter.itemClickListener?.invoke(this) }
+        }
+
+    override fun getItemCount() = presenter.getCount()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        presenter.bindView(holder.apply { pos = position })
+
+    inner class ViewHolder(private val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root),
+        UserItemView {
+        override var pos = -1
+
+        override fun setLogin(text: String) = with(binding) {
+            tvLogin.text = text
+        }
+    }
+}
